b5da38d88817d87fd96263fcd4c7a21f0f15aa62
sasi2400 <sasi2400@gmail.com>
2025-07-18 17:45:59 +0200

    Update extension to v1-2-7
    Backend URL: https://ssrn-summarizer-backend-v1-2-7-pisqy7uvxq-uc.a.run.app
    Extension version: 1.2.7
    Deployment date: Fri Jul 18 17:45:59 CEST 2025
    Features: [Add your feature description here]

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
       enabled: true
     },
     CLOUD_RUN: {
-      url: 'https://ssrn-summarizer-backend-v1-2-6-pisqy7uvxq-uc.a.run.app',
+      url: 'https://ssrn-summarizer-backend-v1-2-7-pisqy7uvxq-uc.a.run.app',
       name: 'Cloud Run',
       priority: 3,
       enabled: true
